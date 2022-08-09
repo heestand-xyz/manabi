ebb2027083975177ec2757dbf3f093ebf0888500
Anton Heestand <anton.heestand@gmail.com>
2022-08-09 19:19:02 +0200

    Initial Commit

---
+++ 学び/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  学び
+//
+//  Created by Anton Heestand on 2022-08-09.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
